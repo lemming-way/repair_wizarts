55897b8bf780c87c26e3f322e29bc00e8fbd64f4
lemming <lemmingway@mail.ru>
2025-09-20 13:29:33 +0500

    Дополнительно удалил некоторые файлы после очистки лишних импортов в коде

--- src/components/Portal/Portal.jsx
+++
@@ -1,7 +0,0 @@
-import { createPortal } from 'react-dom'
-
-const modalRoot = document.getElementById('modal')
-
-const Portal = ({ children }) => createPortal(children, modalRoot)
-
-export default Portal
