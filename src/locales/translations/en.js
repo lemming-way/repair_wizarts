3c0a38141a41885da6a36699f1ae269151aacac0
lemming <lemmingway@mail.ru>
2025-10-03 05:39:49 +0500

    Разделил языковые константы по разным файлам.
    Отдельно для каждого языка. Будет удобнее работать, когда строк станет много.
    TODO: В будущем, возможно, следует сделать отложенную загрузку языковых файлов.

---
+++ src/locales/translations/en.js
@@ -0,0 +1,7 @@
+const lang = {
+  "articles": "Articles",
+  "reviews": "Reviews",
+  "contacts": "Contacts"
+};
+
+export default lang;
--- src/state/language.ts
+++ src/state/language.ts
@@ -1,11 +1,19 @@
 import { useGlobalState, getGlobal, setGlobal } from './global';
-import translations from '../locales/translations.json';
+
+const context = require.context( '../locales/translations', false, /\.js$/ );
+const translations = {};
+
+context.keys().forEach( ( name ) => {
+  const fn = name.substring( name.lastIndexOf( '/' ) + 1 );
+  const lang = fn.substring( 0, fn.lastIndexOf( '.' ) );
+  translations[ lang ] = context( name ).default;
+} );
 
 export type LanguageFn = ( key: string ) => string;
 
 const getLanguageFunction = ( language: string ): LanguageFn => {
   return ( key ) => {
-    return translations[ language ][ key ] || key;
+    return translations[ language ]?.[ key ] || key;
   };
 };
 
