b2edc9b3efd5c22009708ed9a53457845802803c
lemming <lemmingway@mail.ru>
2026-04-04 14:40:26 +0500

    Доработал карту для выбора мастера

---
+++ src/app/components/Map/ContractorDetails.jsx
@@ -0,0 +1,61 @@
+import { useLanguage } from 'app/state/language';
+import { BusinessModel } from 'app/state/user';
+import styles from './Map.module.css';
+
+export function ContractorDetails({ contractor }) {
+  const text = useLanguage();
+  const businessModelText = contractor.businessModel === BusinessModel.ServiceCenter
+    ? text('Service center')
+    : text('Independent technician');
+  
+  return (
+    <div className={styles.balloon}>
+        <div className={styles.balloonHeader}>
+            <div className={styles.balloonHeaderPicture}>
+                <img
+                  src={contractor.avatar || '/img/img-camera.png'}
+                  alt={contractor.fullname}
+                  className={styles.balloonHeaderPictureImg} />
+            </div>
+            <div className={styles.balloonHeaderInfo}>
+                <h3 className={styles.balloonHeaderName}>
+                    {contractor.fullname}
+                </h3>
+                <p className={styles.balloonHeaderType}>{businessModelText}</p>
+                {/* todo: добавить рейтинг мастера
+                <div className={styles.balloonHeaderRate}>
+                    {contractor.rating}
+                </div>
+                */}
+            </div>
+        </div>
+        <div className={styles.balloonBody}>
+            <p className={styles.balloonBodyAddress}>{contractor.address}</p>
+            <div className={styles.balloonBodyBottom}>
+                <div className={styles.balloonBodyInfo}>
+                    <div className={styles.ballonBodyInfoTitle}>
+                        На сайте:
+                    </div>
+                    <div className={styles.ballonBodyInfoContent}>
+                        с 2023 года
+                    </div>
+                    {/* todo: зачем-то второй раз рейтинг. может, что-нибудь другое?
+                    <div className={styles.ballonBodyInfoTitle}>
+                        Оценка:
+                    </div>
+                    <div className={styles.ballonBodyInfoContent}>
+                        {contractor.rating}/5
+                    </div>
+                    */}
+                    <div className={styles.ballonBodyInfoTitle}>
+                        Имя организации:
+                    </div>
+                    <div className={styles.ballonBodyInfoContent}>
+                        {contractor.organizationName}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
--- src/app/components/Map/index.js
+++ src/app/components/Map/index.js
@@ -1,9 +1,10 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useRef } from 'react';
 import ReactDom from 'react-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import { useLanguage } from '../../state/language';
 import { useGlobalState } from '../../state/global';
+import { ContractorDetails } from './ContractorDetails';
 
 function queryYMaps() {
   if (window.ymaps3) {
@@ -42,12 +43,42 @@ function Map(props) {
   const YMaps = useQuery( { queryKey: [ 'YMaps' ], queryFn: queryYMaps, placeholderData: null } ).data;
   const mapLocation = useGlobalState( 'map:location' );
 
+  const [hoveredContractor, setHoveredContractor] = useState(null);
+  const hoverTimeoutRef = useRef(null);
+
+  // useMemo для стабилизации ссылки
   const mapDefaultLocation = useMemo( () => {
     return {
       center: [ mapLocation.longitude, mapLocation.latitude ],
       zoom: 10
     };
   }, [ mapLocation.latitude, mapLocation.longitude ] );
+
+  // todo: это временный блок для назначения фиктивных координат пользователям
+  // позже его нужно будет удалить
+  const contractorsFiction = useMemo( () => {
+    return contractors?.map( contractor => {
+      const longitude = mapDefaultLocation.center[0] + Math.round( 10000 * ( Math.random() * 0.3 - 0.15 ) ) / 10000;
+      const latitude = mapDefaultLocation.center[1] + Math.round( 10000 * ( Math.random() * 0.3 - 0.15 ) ) / 10000;
+
+      return {
+        ...contractor,
+        coordinates: { latitude, longitude }
+      };
+    } );
+  }, [ contractors, mapDefaultLocation ] );
+
+  const handleMouseEnter = id => {
+    if (hoverTimeoutRef.current) clearTimeout(hoverTimeoutRef.current);
+    hoverTimeoutRef.current = null;
+    setHoveredContractor(id);
+  };
+
+  const handleMouseLeave = () => {
+    hoverTimeoutRef.current = setTimeout(() => {
+      setHoveredContractor(null);
+    }, 300); // небольшая задержка перед скрытием
+  };
 
   return (
     <div style={{ width: '100%', height: '500px' }}>
@@ -63,20 +94,27 @@ function Map(props) {
           title={text('You are here')}
           color='red'
         />
-      {/* Перебираем мастеров и создаем для каждого метку */}
-      {/* todo: Добавить детальную информацию по мастерам */}
-      {contractors
-        ?.filter(v => !!v.coordinates)
-        .map((v) => (
-          <YMaps.YMapDefaultMarker
-            key={v.id}
-            coordinates={[v.longitude, v.latitude]}
-            iconName='auto-parts'
-            size='normal'
-            color='red'
-            onClick={() => selectContractor(v)}
-          />
-      ))}
+        {/* Перебираем мастеров и создаем для каждого метку */}
+        {contractorsFiction
+          ?.filter(v => !!v.coordinates)
+          .map((v) => (
+            <YMaps.YMapDefaultMarker
+              key={`${v.id} ${v.coordinates.longitude} ${v.coordinates.latitude}`}
+              coordinates={[v.coordinates.longitude, v.coordinates.latitude]}
+              iconName='auto_parts'
+              size='small'
+              color='green'
+              popup={{
+                content: () => <ContractorDetails contractor={v} />,
+                position: 'bottom',
+                offset: 16,
+                show: hoveredContractor === v.id
+              }}
+              onMouseEnter={() => handleMouseEnter(v.id)}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => selectContractor(v)}
+            />
+        ))}
       </YMaps.YMap>
       :
       text('Yandex maps not loaded')
